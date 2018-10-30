bcabcd1b27f0cf6d1a6fdb143e16bfeb6b392497
Michael Crook <michael.k.crook@gmail.com>
2018-10-30 12:44:43 +0800

    added support for PxPost (to be used for Complete TxnType)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var request = require('request');
 var xml = require('xml');
-// var url = 'https://sec.paymentexpress.com/pxaccess/pxpay.aspx';
+// var pxPayUrl = 'https://sec.paymentexpress.com/pxaccess/pxpay.aspx';
 
 var txtTypes = ['Purchase', 'Auth', 'Complete', 'Refund', 'Validate'];
 var SUCCESS_STATUS = 1;
@@ -8,14 +8,14 @@ var FAIL_STATUS = 0;
 
 var logOutput = false;
 
-module.exports = (function(url) {
+module.exports = (function(pxPayUrl, pxPostUrl) {
     return {
         generateRequest: function (details) {
-            
+
             if (details.logOutput) {
                 logOutput = details.logOutput;
             }
-            
+
             var dataPayload = [
                 { PxPayUserId: details.user },
                 { PxPayKey: details.password },
@@ -68,7 +68,7 @@ module.exports = (function(url) {
             if (logOutput) {
                 console.log(dpsData);
             }
-    
+
             return xml(dpsData);
         },
         generateResponse: function (details) {
@@ -77,7 +77,7 @@ module.exports = (function(url) {
                 { PxPayKey: details.password },
                 { Response: details.response}
             ];
-    
+
             var dpsData = {
                 ProcessResponse: dataPayload
             };
@@ -88,9 +88,9 @@ module.exports = (function(url) {
         },
         request: function (details, callback) {
             var dpsData = this.generateRequest(details);
-    
+
             request({
-                uri: url,
+                uri: pxPayUrl,
                 method: 'POST',
                 body: dpsData
             }, function requestCallback (err, res, body) {
@@ -116,18 +116,18 @@ module.exports = (function(url) {
                             });
                         });
                     }
-    
+
                 });
             });
         },
         process: function () {
-            
+
         },
         parseResults: function(details, callback) {
             var dpsData = this.generateResponse(details);
-    
+
             request({
-                uri: url,
+                uri: pxPayUrl,
                 method: 'POST',
                 body: dpsData
             }, function responseCallback (err, res, body) {
@@ -140,7 +140,7 @@ module.exports = (function(url) {
                         var parser = new require('xml2js').Parser({ explicitArray: false});
                         process.nextTick(function(){
                             parser.parseString(body, function parserHandler (error, result){
-    
+
                                 if (logOutput) {
                                     console.log(error);
                                 }
@@ -154,9 +154,70 @@ module.exports = (function(url) {
                             });
                         });
                     }
-    
+
                 });
             });
+        },
+        generateAuthCompleteRequest: function(details) {
+			if (details.logOutput) {
+				logOutput = details.logOutput;
+			}
+
+			var dataPayload = [
+				{ PostUsername: details.user },
+				{ PostPassword: details.password },
+				{ TxnType: details.transactionType || 'Purchase' },
+				{ AmountInput: details.amount },
+				{ CurrencyInput: details.currency || 'NZD' }
+			];
+			if (details.reference) {
+				dataPayload.push({ MerchantReference: details.reference });
+			}
+			if (details.dps_ref) {
+				dataPayload.push({ DpsTxnRef: details.dps_ref });
+			}
+			var dpsData = {
+				Txn: dataPayload
+			};
+			if (logOutput) {
+				console.log(dpsData);
+			}
+
+			return xml(dpsData);
+        },
+        completeAuthPayment: function(details, callback) {
+			var dpsData = this.generateAuthCompleteRequest(details);
+
+			request({
+				uri: pxPostUrl,
+				method: 'POST',
+				body: dpsData
+			}, function requestCallback (err, res, body) {
+				process.nextTick(function requestHandler () {
+					if(err) {
+						process.nextTick(function(){
+							callback(err);
+						});
+					} else {
+						var parser = new require('xml2js').Parser({ explicitArray: false});
+						process.nextTick(function(){
+							parser.parseString(body, function parserHandler (error, result){
+								if (logOutput) {
+									console.log(error);
+								}
+								if(result.Request.$.valid === '1'){
+									process.nextTick(function(){ callback(null, result.Request); });
+								} else if (error) {
+									process.nextTick(function(){ callback(error); });
+								} else {
+									process.nextTick(function(){ callback({message: "result did not return correct code", result: result });});
+								}
+							});
+						});
+					}
+
+				});
+			});
         }
     };
 });
